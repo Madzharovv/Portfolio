f98cb6bfa3bc034cbdb7388e6559f2c55aa86b0f
“first_name <71776566+Madzharovv@users.noreply.github.com>
2024-05-26 23:57:53 +0100

    Nav Development

---
+++ personal-portfolio/src/Containers/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import {navRoutes} from '../../assets/'
+const Navbar = ()=>{
+
+return (
+<ul>
+    navRoutes.map((route)=>{
+        
+        return(
+            <li><a>href=</a></li>
+        )
+    })
+</ul>
+)
+};
---
+++ personal-portfolio/src/assets/Routes.jsx
@@ -0,0 +1,25 @@
+const navRoutes=[
+    {
+    title: 'Home',
+    href:"#"
+},
+{
+    title: 'About',
+    href:"#"
+},{
+    title: 'Skills',
+    href:"#"
+},{
+    title: 'Projects',
+    href:"#"
+},{
+    title: 'Store',
+    href:"#"
+},{
+    title: 'Contact',
+    href:"#"
+}];
+export default {
+   
+  navRoutes
+  };
