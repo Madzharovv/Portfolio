8a8011fca0eb422330d8c10326b5642f165a0504
“first_name <71776566+Madzharovv@users.noreply.github.com>
2024-05-29 13:36:12 +0100

    Integrated a light and dark toggle button

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -10,12 +10,49 @@ const App = () => {
       setThemeMode(prevMode => (prevMode === 'Dark' ? 'Light' : 'Dark'));
       const lightThemeLogo="images.LightThemeLogo"
       const darkThemeLogo="images.DarkThemeLogo"
-
+      const theme = document.getElementById('theme');
+      const changeTheme = document.getElementById('mode');
+      changeTheme.onchange = (e) => {
+        if (changeTheme.checked === true) {
+          console.log("Checked")
+          theme.innerHTML="Light Theme"
+          document.documentElement.classList.remove("dark")
+          document.documentElement.classList.add("light")
+          window.localStorage.setItem('mode', 'light');
+        } else {
+          console.log("Not Checked")
+          theme.innerHTML="Dark Theme"
+          document.documentElement.classList.remove("light")
+          document.documentElement.classList.add("dark")
+          window.localStorage.setItem('mode', 'dark');
+        }
+      }
+      const mode = window.localStorage.getItem('mode');
+      if (mode == 'dark') {
+        changeTheme.checked = true;
+        document.documentElement.classList.remove("light")
+        document.documentElement.classList.add("dark")
+      }
+      
+      if (mode == 'light') {
+        changeTheme.checked = false;
+        document.documentElement.classList.remove("dark")
+        document.documentElement.classList.add("light")
+      }
   };
     return(
 
     <div className="App">
-      <btn></btn>
+      <div class="container">
+  <label class="toggle_label">
+    <input type="checkbox" id="mode" class="toggle"/>
+    <span class="slider round">
+      <i class="fa-solid fa-moon fa-lg  moon"></i>
+      <i class="fa-solid fa-sun  fa-lg sun"></i>
+    </span>
+  </label>
+  <p id="theme"></p>
+</div>
     <div className='LogoContainer'>
           <img src=""></img>
           </div>
